f6889fbb562ccbb87e6ca602427aeee9f6192d8b
Pranay Ankit <pranayankit414@gmail.com>
2019-10-28 19:12:23 +0530

    add factory method

---
+++ factory_method.py
@@ -0,0 +1,55 @@
+"""
+Implements factory design pattern for reading different types of files like XML, JSON etc. 
+"""
+import xml.etree.ElementTree as etree
+import json
+
+class JSONConnector():
+    def __init__(self, filepath):
+        self.data = dict()
+        with open(filepath, mode='r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
+    @property
+    def parsed_data(self):
+        return self.data
+
+class XMLConnector():
+    def __init__(self, filepath):
+        self.tree = etree.parse(filepath)
+
+    @property
+    def parsed_data(self):
+        return self.tree
+
+def connection_factory(filepath):
+    if filepath.endswith('json'):
+        connector = JSONConnector
+    elif filepath.endswith('xml'):
+        connector = XMLConnector
+    else:
+        raise ValueError('Cannot connect to {}'.format(filepath))
+    return connector(filepath)
+
+def connect_to(filepath):
+    factory = None
+    try:
+        factory = connection_factory(filepath)
+    except ValueError as ve:
+        print(ve)
+    return factory
+
+def main():
+    sqlite_factory = connect_to('data/person.sq3')
+    print()
+
+    xml_factory = connect_to('data/person.xml')
+    xml_data = xml_factory.parsed_data
+    print(xml_data)
+
+    json_factory = connect_to('data/person.json')
+    json_data = json_factory.parsed_data
+    print(json_data)
+
+if __name__ == '__main__':
+    main()
